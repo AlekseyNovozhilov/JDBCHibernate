9f68a5ab650c4d84f978f972eb96648cb40a3ab3
Aleksey <akaleks82novozhilov@yandex.ru>
2023-02-09 09:53:37 +0300

    ver 1.0

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,109 @@
 package jm.task.core.jdbc.dao;
 
-import jm.task.core.jdbc.model.User;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
+    Transaction transaction = null;
+
     public UserDaoHibernateImpl() {
 
     }
 
+    public Session session = Util.getSessionFactory().openSession();
 
     @Override
     public void createUsersTable() {
-
+        String sql = "CREATE TABLE IF NOT EXISTS user (Id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(60), lastName VARCHAR(60), age INT)";
+        try {
+            transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+            System.out.println("Таблица создана.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Ошибка при создании таблицы");
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        String sql = "DROP TABLE IF EXISTS user";
+        try {
+            transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+            System.out.println("Таблица удалена");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Ошибка при удалении таблицы");
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        try {
+            transaction = session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            transaction.commit();
+            System.out.println("Пользователь " + name + " добавлен в базу.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Ошибка добавления пользователя.");
+            transaction.rollback();
+        }
 
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try {
+            transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            transaction.commit();
+            System.out.println("Пользователь " + user.getName() + " удалён.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Ошибка удаления пользователя");
+            transaction.rollback();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        try {
+            transaction = session.beginTransaction();
+            users = session.createQuery("FROM User", User.class).list();
+            transaction.commit();
+            System.out.println("Все пользователи в таблице:");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Ошибка получения пользователей");
+        }
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try {
+            transaction = session.beginTransaction();
+            session.createQuery("DELETE FROM User").executeUpdate();
+            transaction.commit();
+            System.out.println("Таблица очищена.");
+        } catch (Exception e) {
+            transaction.rollback();
+            e.printStackTrace();
+            System.out.println("Ошибка при очистке таблицы");
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,15 +1,27 @@
 package jm.task.core.jdbc.util;
 
 
+
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/usersdbtest";
     private static final String LOGIN = "root";
     private static final String PASSWORD = "dvalin12";
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnect() {
         Connection connection = null;
@@ -22,6 +34,40 @@ public class Util {
             System.out.println("Нет связи с Базой данных!");
         }
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties setting = new Properties();
+                setting.put(Environment.DRIVER, DRIVER);
+                setting.put(Environment.URL, URL);
+                setting.put(Environment.USER, LOGIN);
+                setting.put(Environment.PASS, PASSWORD);
+                setting.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                setting.put(Environment.SHOW_SQL, "true");
+                setting.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                setting.put(Environment.HBM2DDL_AUTO, "");
+
+                configuration.setProperties(setting);
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                System.out.println("Соединени с Базой данных установлено!");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Нет связи с Базой данных!");
+            }
+        }
+        return sessionFactory;
+    }
+
+    public static void closeSessionFactory() {
+        sessionFactory.close();
+        System.out.println("Соединение с Базой данных закрыто.");
     }
 
     // реализуйте настройку соеденения с БД
